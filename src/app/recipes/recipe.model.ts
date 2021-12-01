914b0d32e4014d3c0fe2f98139919f827e430f59
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-01 10:24:21 +0530

    displayed various recipes on front end

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,7 +2,6 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
-  number = 0;
 
   constructor(name: string, description: string, imagePath: string) {
     this.name = name;
